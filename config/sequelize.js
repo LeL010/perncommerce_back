a0956ab9dc51c5eac100208b2ecea9a4704b7eb2
Lel010 <averageguytooversatile@gmail.com>
2023-11-08 22:45:11 +0800

    Probably my last commit for this project for post presentation

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,12 +1,23 @@
 const { Sequelize } = require("sequelize");
 
-// Create a new Sequelize instance
-const sequelize = new Sequelize({
-  username: process.env.DB_USERNAME,
-  password: null,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
-});
+let sequelize;
+
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize({
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+  });
+} else {
+  sequelize = new Sequelize({
+    username: process.env.DB_USERNAME,
+    password: null,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  });
+}
 
 module.exports = sequelize;
